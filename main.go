747af78ebcf4aaa13a6bda138d7e75cd8c0e00a8
Patrick Hrabos <phrabos@protonmail.com>
2023-08-20 19:53:50 +0000

    still figuring out enums

--- main.go
+++ main.go
@@ -2,75 +2,102 @@ package main
 
 import(
   "fmt"
-  "errors"
+  // "errors"
 )
 
-type TeaCategory struct {
-  tea string
-}
+// type TeaCategory struct {
+//   tea string
+// }
  
-var (
-  Green = TeaCategory{"Green"}
-  Black = TeaCategory{"Black"}
-  White = TeaCategory{"White"}
-  Sheng = TeaCategory{"Sheng"}
-  Oolong = TeaCategory{"Oolong"}
-  Heicha = TeaCategory{"Heicha"}
-  Unknown = TeaCategory{"Unknown"}
+// var (
+//   Green = TeaCategory{"Green"}
+//   Black = TeaCategory{"Black"}
+//   White = TeaCategory{"White"}
+//   Sheng = TeaCategory{"Sheng"}
+//   Oolong = TeaCategory{"Oolong"}
+//   Heicha = TeaCategory{"Heicha"}
+//   Unknown = TeaCategory{"Unknown"}
+// )
+
+// func validateTeaCategory(s string) (TeaCategory, error) {
+//   switch s {
+//   case Green.tea:
+//     return Green, nil
+//   case Black.tea:
+//     return Black, nil
+//   case White.tea:
+//     return White, nil
+//   case Sheng.tea:
+//     return Sheng, nil
+//   case Oolong.tea:
+//     return Oolong, nil
+//   case Heicha.tea:
+//     return Heicha, nil
+//   }
+//   return Unknown, errors.New("unknown tea type")
+// }
+
+const (
+  green TeaCategory = iota
+  black
+  white
+  sheng
+  oolong
+  hiecha
 )
 
-func validateTeaCategory(s string) (TeaCategory, error) {
-  switch s {
-  case Green.tea:
-    return Green, nil
-  case Black.tea:
-    return Black, nil
-  case White.tea:
-    return White, nil
-  case Sheng.tea:
-    return Sheng, nil
-  case Oolong.tea:
-    return Oolong, nil
-  case Heicha.tea:
-    return Heicha, nil
-  }
-  return Unknown, errors.New("unknown tea type")
-}
-
-
-var teaCategories = []string{"Green", "Black", "White", "Sheng", "Oolong", "Heicha"}
+type TeaCategory int
+var teaCategoryStrings = []string{"green", "black", "white", "sheng", "oolong", "heicha"}
 
 type tea struct {
   name string
   weight int
   region string
 }
+func (tc TeaCategory) name() string {
+  return teaCategoryStrings[tc]
+}
+
+type Enum interface {
+  name() string
+}
 
 func main() {
   fmt.Println("top of main")
-  fmt.Printf("green %v\n", Green)
-  fmt.Printf("black %v\n", Black)
-  inventory := map[string]tea{}
+  fmt.Printf("green %v\n", green)
+  fmt.Printf("black %v\n", black)
+  inventory := map[TeaCategory]tea{}
 
-  userInput1 := "Oolong"
-  userInput2 := "Black"
+  userInput1 := "oolong"
+  var userInputValidated TeaCategory = indexOf(userInput1, teaCategoryStrings)
+  userInput2 := "black"
   userInput3 := "pink"
+  var userInput3Validated TeaCategory = indexOf(userInput3, teaCategoryStrings)
 
 
-  if _, err := validateTeaCategory(userInput1); err != nil {
-    fmt.Println(err)
-  }
-  if _, err := validateTeaCategory(userInput2); err != nil {
-    fmt.Println(err)
-  }  
-if _, err := validateTeaCategory(userInput3); err != nil {
-    fmt.Println(err)
-  }
+//   if _, err := validateTeaCategory(userInput1); err != nil {
+//     fmt.Println(err)
+//   }
+//   if _, err := validateTeaCategory(userInput2); err != nil {
+//     fmt.Println(err)
+//   }  
+// if _, err := validateTeaCategory(userInput3); err != nil {
+//     fmt.Println(err)
+//   }
 
 
-  inventory[userInput1] = tea{name: "Ali Shan", weight: 150, region: "Taiwan"}
-  inventory[userInput2] = tea{}
-  inventory[userInput3] = tea{}
+  inventory[userInputValidated] = tea{name: "Ali Shan", weight: 150, region: "Taiwan"}
+  inventory[indexOf(userInput2, teaCategoryStrings)] = tea{name: "Jin Jun Mei", weight: 50, region: "China" }
+  inventory[userInput3Validated] = tea{}
 
   fmt.Printf("inv %+v\n", inventory)
+}
+
+func indexOf(needle string, haystack []string) int {
+  for i, val := range haystack {
+    if val == needle {
+      return i
+    }
+  }
+  return -1
 }
